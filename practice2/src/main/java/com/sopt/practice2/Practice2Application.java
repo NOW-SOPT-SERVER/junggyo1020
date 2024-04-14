b71e497a4221f8ef8f68b3ed0518d82f2c5a5c15
Willy <willy_1020@naver.com>
2024-04-14 16:15:06 +0900

    🔎[Practice] Week_2_Server_Seminar

---
+++ practice2/src/main/java/com/sopt/practice2/Practice2Application.java
@@ -0,0 +1,13 @@
+package com.sopt.practice2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practice2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practice2Application.class, args);
+	}
+	System.out.println("Sopt 세미나 2주차 실습 연습!");
+}
---
+++ practice2/src/main/java/com/sopt/practice2/domain/Part.java
@@ -0,0 +1,20 @@
+package com.sopt.practice2.domain;
+
+public enum Part {
+    SERVER("SERVER"),
+    WEB("WEB"),
+    ANDROID("ANDROID"),
+    IOS("IOS"),
+    DESIGN("DESIGN"),
+    PLAN("PLAN"),
+    ;
+    public String part;
+
+    Part(String part) {
+        this.part = part;
+    }
+
+    public String getPart() {
+        return this.part;
+    }
+}
