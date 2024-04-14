21d70e74ffa23395883a6f842240d3c2a77b1d60
Willy <willy_1020@naver.com>
2024-04-14 18:00:24 +0900

    🍊[Assignment] Week_2_멤버리스트 반환하기

--- practice2/src/main/java/com/sopt/practice2/Web/dto/MemberFindDto.java
+++ practice2/src/main/java/com/sopt/practice2/Web/dto/MemberFindDto.java
@@ -2,7 +2,6 @@ package com.sopt.practice2.Web.dto;
 
 import com.sopt.practice2.domain.Member;
 import com.sopt.practice2.domain.Part;
-import lombok.Builder;
 
 public record MemberFindDto(
         String name,
