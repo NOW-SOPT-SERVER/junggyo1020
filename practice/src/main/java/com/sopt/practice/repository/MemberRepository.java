99add0995965f65cafa6ec6ac68301ca1bc1cac4
Willy <willy_1020@naver.com>
2024-04-16 23:03:02 +0900

    파일 정리

--- practice/src/main/java/com/sopt/practice/repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sopt.practice.repository;
-
-import com.sopt.practice.domain.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
