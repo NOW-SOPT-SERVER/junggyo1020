63e43edae3f01ba632300f192421735500128c02
Willy <willy_1020@naver.com>
2024-05-28 16:26:17 +0900

    seminar/#6

---
+++ week6/sopt/src/main/java/org/sopt/Seminar6Application.java
@@ -0,0 +1,13 @@
+package org.sopt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Seminar6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Seminar6Application.class, args);
+	}
+
+}
